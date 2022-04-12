fa93024e5a4e975a5c13864bc1e9ec74b5c2b0fc
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-04-12 17:36:39 -0300

    feat: Model Generico

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,19 @@
+import { Model as M, Document } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
+
+export default class MongoModel<T> implements Model<T> {
+  constructor(protected model: M<T & Document>) {}
+
+  create = async (object: T): Promise<T> => this.model.create({ ...object });
+
+  read = async (): Promise<T[]> => this.model.find();
+
+  readOne = async (id:string): Promise<T | null> =>
+    this.model.findOne({ _id: id });
+
+  update = async (id:string): Promise<T | null> =>
+    this.model.findOneAndUpdate({ _id: id });
+
+  delete = async (id:string): Promise<T | null> =>
+    this.model.findOneAndDelete({ _id: id });
+}
