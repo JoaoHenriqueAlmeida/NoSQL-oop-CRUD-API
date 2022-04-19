5797eaf4d9a54ade8e432d11e0c1ec157f4e2b89
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-04-19 10:14:11 -0300

    abstract class Controller

---
+++ src/controllers/index.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import Services from '../services';
+
+export type ResponseError = {
+  error:unknown;
+};
+
+export interface RequestWithBody<T> extends Request {
+  body: T;
+}
+
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+}
+
+export default abstract class Controller<T> {
+  abstract route: string;
+
+  protected errors = ControllerErrors;
+
+  constructor(protected service: Services<T>) { }
+
+  abstract create(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+
+  read = async (
+    _req: Request,
+    res: Response<T[] | ResponseError>,
+  ):Promise<typeof res> => {
+    try {
+      const objs = await this.service.read();
+      return res.json(objs);
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  abstract readOne(
+    req:Request<{ id:string }>,
+    res:Response<T | ResponseError>
+  ):Promise<typeof res>;
+
+  abstract update(
+    req: RequestWithBody<T>,
+    res:Response<T | ResponseError>,
+  ):Promise<typeof res>;
+
+  abstract delete(
+    req: RequestWithBody<T>,
+    res:Response<T | ResponseError>,
+  ):Promise<typeof res>;
+}
